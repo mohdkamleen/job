264e0824ad9f59eb779bab78128430a441007821
mohdkamleen <kamleenmohd@gmail.com>
2021-07-22 17:41:33 +0530

    dsfadsfsadf

--- infoware/static/js/nav.js
+++ infoware/static/js/nav.js
@@ -1,10 +1,12 @@
 var bars = document.getElementsByClassName("ham_toggle")[0]; 
 var nav = document.getElementsByClassName("nav")[0];  
 var logo = document.getElementsByClassName("logo")[0];  
+var copyright = document.getElementsByClassName("copyright")[0];  
 
 bars.addEventListener("click", function(){ 
     nav.classList.toggle("active"); 
     logo.classList.toggle("active");  
+    copyright.classList.toggle("active");  
     this.classList.toggle("fa-bars"); 
 
 }); 
