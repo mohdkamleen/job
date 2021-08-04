3d32c5012ab8f6bad13abc662edfd263c4924bc3
mohdkamleen <kamleenmohd@gmail.com>
2021-08-04 08:48:51 +0530

    create sw.js file

---
+++ motor/sw.js
@@ -0,0 +1,50 @@
+
+var appVersion = 'v-1.0';
+var files = [
+    './index.html',
+    './state.html',
+    './sw.js'
+];
+
+self.addEventListener("install", event => {
+    event.waitUntil(
+        caches.open(appVersion)
+        .then(cache => {
+            return cache.addAll(files)
+            .catch(err => {
+                console.error('Error adding files to cache', err);
+            })
+        })
+    )
+    console.info('Service Worker is Installed');
+    self.skipWaiting();
+})
+
+
+self.addEventListener("activate", event => {
+    event.waitUntil(
+        caches.keys()
+        .then(cachename => {
+            return Promise.all(
+                cachename.map(cache => {
+                    if(cache !== appVersion){
+                        console.info('delete old caches', cache)
+                        return caches.delete(cache);
+                    }
+                })
+            )
+        })
+        )
+            return self.clients.claim();
+        })
+
+
+        self.addEventListener("fetch", event => {
+            console.info('Service Worker fetch', event.request.url);
+            event.respondWith(
+                caches.match(event.request)
+                .then(res => {
+                    return res || fetch(event.request);
+                })
+            )
+         })
