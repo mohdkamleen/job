1120973bf481393951d91faa87bf67f514d82f8d
mohdkamleen <kamleenmohd@gmail.com>
2021-08-31 13:46:39 +0530

    dfgs

--- getart/index.js
+++ getart/index.js
@@ -58,16 +58,16 @@ conn.query(sql, (err, res) => {
 	console.log("(auth) table created")
 })
 // create table for items  
-var sql = "CREATE TABLE if not exists items (id INT AUTO_INCREMENT PRIMARY KEY, image VARCHAR(255), variantsImg1 VARCHAR(255), variantsImg2 VARCHAR(255), variantsImg3 VARCHAR(255), variantsTxt4 VARCHAR(255),variantsImg4 VARCHAR(255), variantsTxt1 VARCHAR(255),variantsTxt2 VARCHAR(255),variantsTxt3 VARCHAR(255),inCard INT(50), description VARCHAR(255), title VARCHAR(255), rate VARCHAR(255), stockStatus VARCHAR(255))";
+var sql = "CREATE TABLE if not exists items (id INT AUTO_INCREMENT PRIMARY KEY, image VARCHAR(255), variantsImg1 VARCHAR(255), variantsImg2 VARCHAR(255), variantsImg3 VARCHAR(255), variantsTxt4 VARCHAR(255),variantsImg4 VARCHAR(255), variantsTxt1 VARCHAR(255),variantsTxt2 VARCHAR(255),variantsTxt3 VARCHAR(255), description VARCHAR(255), title VARCHAR(255), rate VARCHAR(255), stockStatus VARCHAR(255))";
 conn.query(sql, (err, res) => {
 	console.log("(items) table created")
 })
- 
+
 // var sql = "DROP DATABASE getart";
 // conn.query(sql, (err, res) => {
 // 	console.log("(items) table created")
 // })
- 
+
 
 // get and post urls     
 
@@ -75,12 +75,20 @@ app.get('/', function (request, response) {
 
 
 	if (request.session.loggedin) {
-		conn.query('SELECT * FROM `items`', function (error, results, fields) {
+ 
+		conn.query('SELECT * FROM `auth` where username = ?',[request.session.username], function (error, auth, fields) {
+			conn.query('SELECT * FROM `items`', function (error, results, fields) {
+				var userName = "";
+				auth.forEach(element => {
+					userName = element.name
+				});
 			response.render("index", {
-				title: "| Dashboard",
+				title: "| Dashboard", 
 				data: results,
-				user: request.session.username
+				user: request.session.username,
+				userName: userName
 			})
+		})
 		})
 
 	} else {
@@ -89,12 +97,12 @@ app.get('/', function (request, response) {
 			if (results.length > 0) {
 				response.render("index", {
 					title: "| Dashboard",
-					data: results 
+					data: results
 				})
 			} else {
 				response.render("index", {
 					title: "| Dashboard",
-					message: "Not avilable items" 
+					message: "Not avilable items"
 				})
 			}
 		})
@@ -145,9 +153,14 @@ app.get('/forgot', function (req, res) {
 })
 
 app.get('/itemdelete/:id', function (req, res) {
-	conn.query('DELETE FROM `items` WHERE id = ?', [req.params.id], function (error, results, fields) {
+	if (req.session.loggedin) {
+	conn.query('DELETE FROM '+ req.session.username +' WHERE id = ?', [req.params.id], function (error, results, fields) {
+		conn.query('DELETE FROM `items` WHERE id = ?', [req.params.id]);
 		res.redirect("/admin")
 	})
+} else {
+	res.redirect("/login");
+}
 })
 
 app.get('/userdelete/:id', function (req, res) {
@@ -174,7 +187,7 @@ app.post('/login', function (request, response) {
 				request.session.loggedin = true;
 				request.session.username = request.body.username;
 				response.redirect('/');
-
+ 
 				var sql = "CREATE TABLE if not exists " + request.session.username + " (id INT AUTO_INCREMENT PRIMARY KEY, image VARCHAR(255), variant VARCHAR(255),inCard INT(50), description VARCHAR(255), title VARCHAR(255), quantity VARCHAR(255), rate VARCHAR(255))";
 				conn.query(sql, (err, res) => {
 					console.log(request.session.username + " table created")
@@ -202,7 +215,7 @@ app.post('/signup', function (request, response) {
 			if (results.length > 0) {
 				response.send('UserName alredy exist');
 			} else {
-				conn.query("INSERT INTO `auth`(`name`, `username`, `password`) VALUES (?,?,?)", [name, username, password], function (err,results, res) {
+				conn.query("INSERT INTO `auth`(`name`, `username`, `password`) VALUES (?,?,?)", [name, username, password], function (err, results, res) {
 					if (err) throw err;
 					console.log(name, 'is created');
 
@@ -245,52 +258,68 @@ app.get('/insert', function (req, res) {
 app.get('/cart', function (req, res) {
 
 	if (req.session.loggedin) {
-		conn.query('SELECT * FROM '+  req.session.username +' WHERE inCard = ?',[1], function (error, results, fields) {
+		conn.query('SELECT * FROM ' + req.session.username + ' WHERE inCard = ?', [1], function (error, results, fields) {
 			res.render("cart", {
 				title: "| Dashboard",
 				data: results,
 				user: req.session.username
 			})
-		}) 
+		})
 
 	} else {
 		res.redirect("/")
 	}
 
-}) 
+})
 
 app.post('/insert', function (request, response) {
 
 
-	if(request.session.loggedin){
-	var title = request.files.title;
-	var description = request.body.description;
-	var rate = request.body.rate;
-	var file = request.files.product_image;
-	var variantsImg1 = request.files.variantsImg1;
-	var variantsImg2 = request.files.variantsImg2;
-	var variantsImg3 = request.files.variantsImg3;
-	var variantsImg4 = request.files.variantsImg4;
-	var variantsTxt1 = request.body.variantsTxt1;
-	var variantsTxt2 = request.body.variantsTxt2;
-	var variantsTxt3 = request.body.variantsTxt3;
-	var variantsTxt4 = request.body.variantsTxt4; 
-	var product_status = request.body.product_status;
-	var inCard = 0;
+	if (request.session.loggedin) {
+		var title = request.body.title;
+		var description = request.body.description;
+		var rate = request.body.rate;
+		var file = request.files.product_image;
+		var variants1Img = request.files.variants1Img;
+		var variants2Img = request.files.variants2Img;
+		var variants3Img = request.files.variants3Img;
+		var variants4Img = request.files.variants4Img;
+		var variantsTxt1 = request.body.variantsTxt1;
+		var variantsTxt2 = request.body.variantsTxt2;
+		var variantsTxt3 = request.body.variantsTxt3;
+		var variantsTxt4 = request.body.variantsTxt4;
+		var product_status = request.body.product_status; 
+
+
+		
+			
+		if (variants1Img) {
+			variants1Img.mv('static/product_image/' + variants1Img.name);
+			variants1Img = "/product_image/"+variants1Img.name;
+		}
+		if (variants2Img) {
+			variants2Img.mv('static/product_image/' + variants2Img.name);
+			variants2Img = "/product_image/"+variants2Img.name;
+		}
+		if (variants3Img) {
+			variants3Img.mv('static/product_image/' + variants3Img.name);
+			variants3Img = "/product_image/"+variants3Img.name;
+		}
+		if (variants4Img) {
+			variants4Img.mv('static/product_image/' + variants4Img.name);
+			variants4Img = "/product_image/"+variants4Img.name;
+		}
  
-	conn.query('INSERT INTO '+request.session.username+'(`image`, `inCard`, `description`, `title`, `rate`, `quantity`) VALUES (?,?,?,?,?,?)', ['/product_image/'+file.name, inCard, description,title, rate, 1])
-		// file.mv('static/product_image/' + file.name, function (err) { 
-			conn.query('INSERT INTO `items`(`image`, `variantsImg1`, `variantsImg2`, `variantsImg3`, `variantsImg4`, `variantsTxt1`, `variantsTxt2`, `variantsTxt3`, `variantsTxt4`, `inCard`, `description`, `title`, `rate`, `stockStatus`) VALUES (?,?,?,?,?,?,?,?,?,?,?,?,?,?)', ['/product_image/'+file.name, '/product_image/'+ variantsImg1.name, '/product_image/'+ variantsImg2.name, '/product_image/'+ variantsImg3.name, '/product_image/'+ variantsImg4.name,variantsTxt1,variantsTxt2,variantsTxt3,variantsTxt4,inCard, description,title, rate,product_status], function (err, red, fields) {
-				if (err) throw err;
-				// variants1.mv('static/product_image/' + variantsImg1.name);
-				// variantsImg2.mv('static/product_image/' + variantsImg2.name);
-				// variantsImg3.mv('static/product_image/' + variantsImg3.name);
-				// variantsImg4.mv('static/product_image/' + variantsImg4.name);
-				response.redirect("/admin")
-			})
-		// })  
+
+
+		conn.query('INSERT INTO `items` (`image`, `variantsImg1`, `variantsImg2`, `variantsImg3`, `variantsImg4`, `variantsTxt1`, `variantsTxt2`, `variantsTxt3`, `variantsTxt4`, `description`, `title`, `rate`, `stockStatus`) VALUES (?,?,?,?,?,?,?,?,?,?,?,?,?)', ['/product_image/' + file.name, variants1Img, variants2Img, variants3Img, variants4Img, variantsTxt1, variantsTxt2, variantsTxt3, variantsTxt4, description, title, rate, product_status], function (err, red, fields) {
+			if (err) throw err; 
+			conn.query('INSERT INTO '+request.session.username+'(`image`, `inCard`, `description`, `title`, `rate`, `quantity`) VALUES (?,?,?,?,?,?)', ['/product_image/'+file.name, 0, description,title, rate, 1])
+			file.mv('static/product_image/' + file.name); 
+			response.redirect("/admin");
+		}) 
 	} else {
-		response.redirect("/login"); 
+		response.redirect("/login");
 	}
 });
 
@@ -312,16 +341,30 @@ app.post('/userupdate', function (request, response) {
 app.post('/updateIntoCart', function (request, response) {
 	if (request.session.loggedin) {
 
-	var id = request.body.id;
-	var variant = request.body.variant; 
+		var id = request.body.id;
+		var variant = request.body.variant;
 
-	conn.query('UPDATE '+request.session.username+' SET variant=?,inCard = ? WHERE id=? ', [variant,1, id], function (error, results, fields) {
-		if (error) throw error;
-		response.redirect("/") 
-	});
-}else{
-	response.redirect("/login") 
-}
+		conn.query('UPDATE ' + request.session.username + ' SET variant=?,inCard = ? WHERE id=? ', [variant, 1, id], function (error, results, fields) {
+			if (error) throw error;
+			response.redirect("/")
+		});
+	} else {
+		response.redirect("/login")
+	}
+});
+
+app.post('/removeItemFromCart', function (request, response) {
+	if (request.session.loggedin) {
+
+		var id = request.body.id; 
+
+		conn.query('UPDATE ' + request.session.username + ' SET inCard = ? WHERE id=? ', [0, id], function (error, results, fields) {
+			if (error) throw error;
+			response.redirect("/cart")
+		});
+	} else {
+		response.redirect("/login")
+	}
 });
 
 
